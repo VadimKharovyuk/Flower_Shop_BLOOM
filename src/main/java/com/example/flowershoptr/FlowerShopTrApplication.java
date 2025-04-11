aa71cc19c8984b5014702bd0cd48539b406efcc9
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2025-04-11 16:44:43 +0300

    Initial commit

---
+++ src/main/java/com/example/flowershoptr/FlowerShopTrApplication.java
@@ -0,0 +1,13 @@
+package com.example.flowershoptr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowerShopTrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowerShopTrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/flowershoptr/FlowerShopTrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flowershoptr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerShopTrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
