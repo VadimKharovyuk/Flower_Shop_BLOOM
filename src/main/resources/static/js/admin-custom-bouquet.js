ba7080591cdbc27bf1f4ea2cafb6de0c0d798768
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2025-04-15 23:38:10 +0300

    add custom-bouquet.js}

--- src/main/resources/static/js/admin-custom-bouquet.js
+++ src/main/resources/static/js/admin-custom-bouquet.js
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const closeModalBtn = document.querySelector('.close');
     const updateStatusBtn = document.getElementById('updateStatusBtn');
 
+
     // ID текущей выбранной заявки
     let currentRequestId = null;
 
