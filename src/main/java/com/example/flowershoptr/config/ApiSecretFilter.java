502da16cd3c4f91ac0b916df4ed2c286303d03d4
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2025-04-25 12:01:36 +0300

    update ApiSecretFilter V1

--- src/main/java/com/example/flowershoptr/config/ApiSecretFilter.java
+++ src/main/java/com/example/flowershoptr/config/ApiSecretFilter.java
@@ -4,13 +4,14 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-
+@RequiredArgsConstructor
 //  для фильтрации запросов на Render API
 @Component
 public class ApiSecretFilter extends OncePerRequestFilter {
